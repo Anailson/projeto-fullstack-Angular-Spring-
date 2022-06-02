120d8127cde29319ade0d9f472ecad142b9ef3c5
anailson ribeiro <arsantos315@gmail.com>
2022-06-02 14:21:56 -0300

    Implementando segurança com OAuth 2 e Password Flow

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d2/6084a14cd5e1001c18f8991082a160b3
@@ -0,0 +1,39 @@
+package com.example.algamoney.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+		 .withUser("admin").password("{noop}admin").roles("ROLE");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	
+		/**Configuração de autenticação com liberação para o endpoint /category e restrição
+		com autenticação básica para os demais. Sem gravação de sessão, não mantém estado (stateless)
+ 		Com Cross Site Request Forgery desabilitado
+		 * 
+		 */
+		http.authorizeRequests()
+		     .antMatchers("/categorias").permitAll()
+		     .anyRequest().authenticated()
+		     .and()
+		     .httpBasic().and()
+		     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		     .csrf().disable();
+		     
+	}
+
+}
---
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -0,0 +1,52 @@
+package com.example.algamoney.api.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configurable
+@EnableAuthorizationServer
+public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter{
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+		clients.inMemory()
+				.withClient("angular")//nome do cliente
+				.secret("@ngul@r0")
+				.scopes("read", "write")//scope limpar o acesso do cliente angular
+				.authorizedGrantTypes("password")
+				.accessTokenValiditySeconds(1800);//tempo para cessar app
+							
+				
+	}	
+	
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		
+		endpoints  
+		    .tokenStore(tokenStore())
+		    .authenticationManager(authenticationManager);
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		
+		return new InMemoryTokenStore();		
+	}
+}
